7ea7fb2299f7b06d336730571ac17ea885b85bd5
Septian Adhitya <dsrisk911@gmail.com>
2022-06-19 13:37:43 +0700

    add navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,29 @@ import React from "react";
 import "./navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="saep__navbar">
+      <div className="saep__navbar-links">
+        <div className="saep__navbar-links_logo">
+          <p>Septianadhitya</p>
+        </div>
+        <div className="saep__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#projects">Projects</a>
+          </p>
+          <p>
+            <a href="#whyHireMe">Why Hire Me?</a>
+          </p>
+          <p>
+            <a href="#skills">Skills</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
