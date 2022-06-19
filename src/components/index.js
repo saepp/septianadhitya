6b6357ef111b2de20bf7f003cba26fafbf7982fd
Septian Adhitya <dsrisk911@gmail.com>
2022-06-19 12:58:44 +0700

    add index.js on containers and components

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as CTA } from "./cta/CTA";
+export { default as Navbar } from "./navbar/Navbar";
+export { default as Project } from "./project/Project";
+export { default as Skill } from "./skill/Skill";
---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export { default as Card } from "./card/Card";
+export { default as Footer } from "./footer/Footer";
+export { default as Header } from "./header/Header";
+export { default as Modal } from "./modal/Modal";
+export { default as Projects } from "./projects/Projects";
+export { default as Skills } from "./skills/Skills";
