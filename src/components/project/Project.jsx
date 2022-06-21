4b2b3c039d61cf8cdf745e5df3b1083e70c1b508
Septian Adhitya <dsrisk911@gmail.com>
2022-06-21 13:11:16 +0700

    add project component

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,8 +1,44 @@
 import React from "react";
+import { BsLink45Deg, BsGithub } from "react-icons/bs";
 import "./project.css";
 
-const Project = () => {
-  return <div>Project</div>;
+const Project = ({ project, name, link, github }) => {
+  return (
+    <div className="saep__project">
+      <div className="saep__project-image">
+        <img src={project} alt="project" />
+      </div>
+      <div className="saep__project-name">
+        <p>{name}</p>
+      </div>
+      <div className="saep__project-links">
+        <div className="saep__project-links_visit">
+          <p>
+            <a href={link}>
+              <div className="wrap__visit">
+                <span className="link-icon">
+                  <BsLink45Deg size={20} color="#fff" />
+                </span>
+                <span className="link-icon-text">Visit Website</span>
+              </div>
+            </a>
+          </p>
+        </div>
+        <div className="saep__project-links_github">
+          <p>
+            <a href={github}>
+              <div className="wrap__github">
+                <span className="github-icon">
+                  <BsGithub color="#fff" />
+                </span>
+                <span className="github-icon-text">Link Github</span>
+              </div>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
--- src/containers/projects/Projects.jsx
+++ src/containers/projects/Projects.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import Project from "../../components/project/Project";
+import project1 from "../../assets/project1.png";
+import project2 from "../../assets/project2.png";
 import "./projects.css";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="saep__projects section__padding" id="projects">
+      <div className="saep__projects-heading">
+        <h4>Related Projects</h4>
+      </div>
+      <div className="saep__projects-project">
+        <Project
+          project={project1}
+          name="cekrs"
+          link="https://cekrs-com.vercel.app"
+          github="https://github.com/saepp/cekrs.com"
+        />
+        <Project
+          project={project2}
+          name="Flip Book"
+          link="https://flip-book-delta.vercel.app"
+          github="https://github.com/saepp/flip-book"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
