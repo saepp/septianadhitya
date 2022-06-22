52ac0a11d28e9d419615c859152acd8cb9c5ebb0
Septian Adhitya <dsrisk911@gmail.com>
2022-06-22 15:36:47 +0700

    add ornament

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Card, Footer, Header, Modals, Projects, Skills } from "./containers";
 import { Navbar } from "./components";
 import ellips1 from "./assets/ellips7.png";
 import ellips2 from "./assets/ellips4.png";
+import ellips3 from "./assets/ellips6.png";
 
 function App() {
   return (
@@ -12,12 +13,15 @@ function App() {
       </div>
       <Navbar />
       <Header />
-      <Projects />
-      <Card />
       <div className="ellips2">
         <img src={ellips2} alt="ellips2" />
       </div>
+      <Projects />
+      <Card />
       <Modals />
+      <div className="ellips3">
+        <img src={ellips3} alt="ellips3" />
+      </div>
       <Skills />
       <Footer />
     </div>
